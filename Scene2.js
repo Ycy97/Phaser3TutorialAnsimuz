6c6625e275a3fca48c5af4f3a027be9335817b61
Yap Cheng Yee <chengyeeyap97@gmail.com>
2023-11-18 20:33:26 +0800

    Moving Images and Scrolling TileSprites

--- Scene2.js
+++ Scene2.js
@@ -5,7 +5,10 @@ class Scene2 extends Phaser.Scene{
     }
 
     create(){
-        this.background = this.add.image(0,0,"background");
+        //modifying image -> tileSprite in order for background to scroll
+        //this.background = this.add.image(0,0,"background");
+
+        this.background = this.add.tileSprite(0, 0, config.width, config.height, "background");
         this.background.setOrigin(0,0);
 
         this.ship1 = this.add.image(config.width / 2 - 50, config.height / 2, "ship");
@@ -13,5 +16,27 @@ class Scene2 extends Phaser.Scene{
         this.ship3 = this.add.image(config.width / 2 + 50, config.height / 2, "ship3");
 
         this.add.text(20,20, "Playing game", {font : "25px Arial", fill : "yellow"});
+    }
+
+    update(){
+        this.moveShip(this.ship1, 1);
+        this.moveShip(this.ship2, 2);
+        this.moveShip(this.ship3, 3);
+
+        this.background.tilePositionY -= 0.5;
+    }
+
+    //function to move the ship on the y-axis
+    moveShip(ship, speed){
+        ship.y += speed;
+        if(ship.y > config.height){
+            this.resetShipPos(ship);
+        }
+    }
+
+    resetShipPos(ship){
+        ship.y = 0;
+        var randomX = Phaser.Math.Between(0, config.width);
+        ship.x = randomX;
     }
 }
