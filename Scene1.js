036ca73811ebb486dc1ae5bb617caf5566d8d13b
Yap Cheng Yee <chengyeeyap97@gmail.com>
2023-11-22 21:59:52 +0800

    Tutorial 8 - Custom Classes (Beams)

--- Scene1.js
+++ Scene1.js
@@ -29,6 +29,10 @@ class Scene1 extends Phaser.Scene {
         frameWidth: 16,
         frameHeight: 24
       });
+      this.load.spritesheet("beam", "assets/spritesheets/beam.png",{
+        frameWidth: 16,
+        frameHeight: 16
+      });
     }
   
     create() {
@@ -88,6 +92,13 @@ class Scene1 extends Phaser.Scene {
       this.anims.create({
         key: "thrust",
         frames: this.anims.generateFrameNumbers("player"),
+        frameRate:20,
+        repeat:-1
+      });
+
+      this.anims.create({
+        key: "beam_anim",
+        frames: this.anims.generateFrameNumbers("beam"),
         frameRate:20,
         repeat:-1
       });
--- Scene2.js
+++ Scene2.js
@@ -57,6 +57,7 @@ class Scene2 extends Phaser.Scene{
 
         //spacebar to shoot
         this.spacebar = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        this.projectiles = this.add.group();
     }
 
     update(){
@@ -70,6 +71,12 @@ class Scene2 extends Phaser.Scene{
 
         if(Phaser.Input.Keyboard.JustDown(this.spacebar)){
             console.log("Fire!");
+            this.shootBeam();
+        }
+
+        for(var i = 0; i < this.projectiles.getChildren().length; i++){
+            var beam = this.projectiles.getChildren()[i];
+            beam.update();
         }
     }
 
@@ -109,5 +116,9 @@ class Scene2 extends Phaser.Scene{
     destroyShip(pointer, gameObject){
         gameObject.setTexture("explosion");
         gameObject.play("explode");
+    }
+
+    shootBeam(){
+        var beam = new Beam(this);
     }
 }
